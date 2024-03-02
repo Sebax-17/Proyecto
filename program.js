a350611aa433f312374c176cb1e20f4fe464d8bc
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-03-02 14:26:46 -0500

    segundo intentoo

--- program.js
+++ program.js
@@ -1,5 +1,9 @@
 const readlineSync = require('readline-sync');
 
 // Wait for user's response.
-let Continuar = readlineSync.question('Si= 1 , No= 0');
-console.log(`continuar`);
+let Continuar = +readlineSync.question('Si= 1 , No= 0');
+if (Continuar == 1){
+
+
+    console.log(Continuar);
+}
