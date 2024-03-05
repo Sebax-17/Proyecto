987045f5cc62b88c58687578ba793f73180641fe
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-03-05 16:21:46 -0500

    Intento de avance 1

--- program.js
+++ program.js
@@ -1,13 +1,36 @@
 const readlineSync = require('readline-sync');
-
+let sueldo = 0;
+let cantidaddehijos = 0;
+let genero = ``;
+let estrato = ``;
+let Zona = ``;
+const Nacionalidad = readlineSync.question(`Ingrese La Nacionalidad En La que se encuentra la empresa`)
 // Wait for user's response.
-let Continuar = +readlineSync.question('Si= 1 , No= 0');
+let Continuar = +readlineSync.question('Si= 1 , No= 0 :  ');
 if (Continuar == 1){
 
 
     console.log(Continuar);
 }
 if(Continuar != 1 & Continuar != 0){
-    console.error("Numeros invalidos ")
+    console.error(`Numeros invalidos` );
+        continue;
     
+}
+while (Continuar == 1 ){
+   if (Nacionalidad == Nacionalidad){
+     sueldo = +readlineSync.question(`Ingresa El Sueldo `)
+     estrato = +readlineSync.question(`Ingresa El Estrato En la Que Vives`)
+     cantidaddehijos= +readlineSync.question(`Cuantos Hijos tiene`)
+     if (cantidaddehijos > 1){
+        let cdehijosprimaria = +readlineSync.question("cuantos hijos estudian en primaria: ")
+        i
+     }
+
+
+     
+   }
+
+
+
 }
