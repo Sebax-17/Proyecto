d33fcd67e2e7412456a6aa2721abfa7add531d0d
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-03-10 17:15:36 -0500

    fal

--- program.js
+++ program.js
@@ -4,15 +4,12 @@ let cantidaddehijos = 0;
 let genero = ``;
 let estrato = ``;
 let Zona = ``;
+let Mujercontador = 0;
 const Nacionalidad = readlineSync.question(`Ingrese La Nacionalidad En La que se encuentra la empresa`)
 
-if (Continuar == 1){
-
-
-    console.log(Continuar);
-}
 while (Continuar == 1 )
    if (Nacionalidad == Nacionalidad){
+     genero = readlineSync.question(`ingresa tu genero M ó H :`)
      sueldo = +readlineSync.question(`Ingresa El Sueldo `)
      estrato = +readlineSync.question(`Ingresa El Estrato En la Que Vives`)
      hijos = readlineSync.question(`tienes Hijos`)
@@ -50,25 +47,23 @@ while (Continuar == 1 )
             sueldo = sueldo * 0.05
         }
        if (hijos == `si` && hijos == `SI` && hijos == `Si`){
-        cdehijosprimaria = +readlineSync.question(`Cantidad de hijos que estudian en primaria`)
-     cdehijossecundaria = +readlineSync.question(`ingresa la cantida de estudian en secundaria`)
-     candehijosUniversida = +readlineSync.question(`Cuantos Hijos estudian en Univesidad`)
-     subsidioprimaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en primaria`)
-     subsidiosecundaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en secundaria`)
-     subsidioUniversidad = +readlineSync.question(`Ingrese el subsidio de tenre hijos en Universidad`)
+         cdehijosprimaria = +readlineSync.question(`Cantidad de hijos que estudian en primaria`)
+         cdehijossecundaria = +readlineSync.question(`ingresa la cantida de estudian en secundaria`)
+         candehijosUniversida = +readlineSync.question(`Cuantos Hijos estudian en Univesidad`)
+         subsidioprimaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en primaria`)
+         subsidiosecundaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en secundaria`)
+         subsidioUniversidad = +readlineSync.question(`Ingrese el subsidio de tenre hijos en Universidad`)
        }
-
+       Sudsidiofinalprimaria = sueldo + (subsidioprimaria* cdehijosprimaria)
+       SudsidiofinalSecundaria = sueldo + (subsidiosecundaria * cdehijossecundaria)
+       SudsidiofinalUniversidad = sueldo + (SudsidiofinalUniversidad * candehijosUniversida)
+        
         
     }
      
-        
-     
-
-
-     
-   
-
-
-
+    console.log(sueldo)
 
 let Continuar = +readlineSync.question('Si= 1 , No= 0 :  ');
+       if(genero == `M`){
+        console
+       }
