f11d45f44eca05686b2cf1fb319f244006964190
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-03-09 20:45:36 -0500

    Avance de intento 2

--- program.js
+++ program.js
@@ -5,42 +5,70 @@ let genero = ``;
 let estrato = ``;
 let Zona = ``;
 const Nacionalidad = readlineSync.question(`Ingrese La Nacionalidad En La que se encuentra la empresa`)
-// Wait for user's response.
-let Continuar = +readlineSync.question('Si= 1 , No= 0 :  ');
+
 if (Continuar == 1){
 
 
     console.log(Continuar);
 }
-if(Continuar !== 1 && Continuar !== 0 ){
-
-    console.error(`Numeros invalidos`)
-   
-    
-}
-while (Continuar == 1 ){
+while (Continuar == 1 )
    if (Nacionalidad == Nacionalidad){
      sueldo = +readlineSync.question(`Ingresa El Sueldo `)
      estrato = +readlineSync.question(`Ingresa El Estrato En la Que Vives`)
-     cantidaddehijos= +readlineSync.question(`Cuantos Hijos tiene`)
-     if (cantidaddehijos > 1){
-        let cdehijosprimaria = +readlineSync.question("cuantos hijos estudian en primaria: ")
-        
-     } if (cdehijosprimaria < cantidaddehijos) {
-        let cdehijossecundaria = +readlineSync.question(`ingresa la cantina de estudian en secundaria`)
+     hijos = readlineSync.question(`tienes Hijos`)
+     
+     subsidioprimaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en primaria`)
+     subsidiosecundaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en secundaria`)
+     subsidioUniversidad = +readlineSync.question(`Ingrese el subsidio de tenre hijos en Universidad`)
+       
+        if (isNaN(cdehijosprimaria)){
+            console.error(`Tiene que ser numerico `)
+            continue;
+        }
         if (isNaN(cdehijossecundaria)){
             console.error(`Tiene que ser numerico `)
             continue;
         }
+        if (isNaN(candehijosUniversida)){
+            console.error(`Tiene que ser numerico `)
+            continue;
+        }
+        
      
+   
+        sueldo = +readlineSync.question(`Ingresa El Sueldo `)
+        estrato = +readlineSync.question(`Ingresa El Estrato En la Que Vives`)
+        let zona = readlineSync.question(`Vive en Rural o Urbano`)
+        if (zona == `Rural`){
+            sueldo = sueldo + 35000;
+        }
+        if (estrato == 1){
+            sueldo = sueldo * 0.15
+        }if (estrato == 2 ) {
+            sueldo = sueldo * 0.1
+        }if (estrato == 3){
+            sueldo = sueldo * 0.05
+        }
+       if (hijos == `si` && hijos == `SI` && hijos == `Si`){
+        cdehijosprimaria = +readlineSync.question(`Cantidad de hijos que estudian en primaria`)
+     cdehijossecundaria = +readlineSync.question(`ingresa la cantida de estudian en secundaria`)
+     candehijosUniversida = +readlineSync.question(`Cuantos Hijos estudian en Univesidad`)
+     subsidioprimaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en primaria`)
+     subsidiosecundaria = +readlineSync.question(`Ingrese el subsidio de tenre hijos en secundaria`)
+     subsidioUniversidad = +readlineSync.question(`Ingrese el subsidio de tenre hijos en Universidad`)
+       }
+
+        
     }
+     
         
      
 
 
      
-   }
+   
 
 
 
-}
+
+let Continuar = +readlineSync.question('Si= 1 , No= 0 :  ');
